ee73babb5ced1b8f79c47048a8665ad8b0bb8b24
Suresh5295 <dinu05021995@gmail.com>
2024-04-29 19:27:30 +0530

    1st commit made

---
+++ script.js
@@ -0,0 +1,74 @@
+// Imagen slider
+  let currentImageIndex = 0;
+  const images = document.querySelectorAll('.imgcontainer img');
+  const arrowRight = document.querySelector('.ar2');
+  const arrowLeft = document.querySelector('.ar1');
+
+  arrowRight.addEventListener('click', function() {
+    images[currentImageIndex].style.display = 'none';
+    currentImageIndex = (currentImageIndex + 1) % images.length;
+    images[currentImageIndex].style.display = 'block';
+  });
+
+  arrowLeft.addEventListener('click', function() {
+    images[currentImageIndex].style.display = 'none';
+    currentImageIndex = (currentImageIndex + 1) % images.length;
+    images[currentImageIndex].style.display = 'block';
+  });
+
+
+  // let ImageIndex = 0;
+  // const proimages = document.querySelectorAll('.posimg img');
+  // const arrow1 = document.getElementById("arrowright");
+  // const arrow2 = document.getElementById("arrowleft");
+
+  // arrow1.addEventListener('click', function() {
+  //   proimages[ImageIndex].style.display = 'none';
+  //   ImageIndex = (ImageIndex + 1) % proimages.length;
+  //   proimages[ImageIndex].style.display = 'block';
+  // });
+  // arrow2.addEventListener('click', function() {
+  //   proimages[ImageIndex].style.display = 'none';
+  //   ImageIndex = (ImageIndex + 1) % proimages.length;
+  //   proimages[ImageIndex].style.display = 'block';
+  // });
+
+  let imageIndex = 0;
+  
+       
+        const proImages = document.querySelectorAll('.posimg img');
+        const arrow1 = document.getElementById('arrowleft');
+        const arrow2 = document.getElementById('arrowright');
+
+        arrow1.addEventListener('click', function() {
+            proImages[imageIndex].style.display = 'none';
+            imageIndex = (imageIndex + 1) % proImages.length;
+            proImages[imageIndex].style.display = 'block';
+        });
+        
+
+        arrow2.addEventListener('click', function() {
+            proImages[imageIndex].style.display = 'none';
+            imageIndex = (imageIndex - 1 + proImages.length) % proImages.length;
+            proImages[imageIndex].style.display = 'block';
+        });
+
+        let headIndex = 0;
+        let scrollInterval;
+        const proheading = document.querySelectorAll('.postit h1');
+        const arr1 = document.getElementById('arrowleft');
+        const arr2 = document.getElementById('arrowright');
+
+        arr1.addEventListener('click', function() {
+        
+          proheading[headIndex].style.display = 'none';
+          headIndex = (headIndex + 1) % proheading.length;
+            proheading[headIndex].style.display = 'block';
+        }
+      );
+      
+
+ 
+   
+   
+    
